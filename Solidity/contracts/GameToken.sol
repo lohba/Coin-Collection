f74a91dd470912347e5c44bf207a04f44d9ebf4d
Luke Ohba <lukeohba@gmail.com>
2020-10-29 00:54:02 -0400

    solidity

---
+++ Solidity/contracts/GameToken.sol
@@ -0,0 +1,16 @@
+pragma solidity 0.6.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+
+contract GameToken is ERC20 {
+    constructor(string memory _name, string memory _symbol) 
+    ERC20(_name, _symbol) 
+    public
+    {}
+
+    function mint(address to, uint256 value) public returns (bool){
+        _mint(to, value);
+        return true;
+    }
+}
